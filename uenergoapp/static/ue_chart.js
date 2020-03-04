17e91ee6f8818d38df991f1cccdfe003c754ad82
mazuraktaras <53152688+mazuraktaras@users.noreply.github.com>
2020-03-04 22:13:01 +0200

    Small corrections in JS

--- uenergoapp/static/ue_chart.js
+++ uenergoapp/static/ue_chart.js
@@ -1,14 +1,6 @@
+let dataPoints = [{label: "div", y: 14}];
 window.onload = function () {
 
-    $('#inputURL').bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        }
-    });
-
-    let dataPoints = [];
 
     let chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
@@ -44,6 +36,26 @@ window.onload = function () {
         }]
     });
 
+
+    $('#ajax-button').click(function () {
+        //dataPoints = [{label: "div", y: 200}];
+        console.log('DATAPOINTS', dataPoints);
+        //event.preventDefault();
+        //startTask($('#url').val());
+        chart.render();
+
+    });
+
+    $('#inputURLform').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        }
+    });
+
+
+    /*
     function addData(data) {
         for (let i = 0; i < data.length; i++) {
             dataPoints.push({
@@ -56,9 +68,74 @@ window.onload = function () {
 
     }
 
-    //$.getJSON("/tagscount", addData);
+
+     */
+
+//$.getJSON("/tagscount", addData);
 
 
+    function getTaskResult(status_url) {
+
+        $.getJSON(status_url, function (data) {
+            console.log('DATAPOINTS', dataPoints)
+            if (data.task_state != 'PENDING') {
+                console.log('Task state ===>', data.task_state);
+                console.log('Task state ===>', data.result_url);
+
+                $.getJSON(data.result_url, function (data) {
+                    $.each(data, function (key, value) {
+                        dataPoints.push({label: value.tag_name, y: value.tag_count});
+                        console.log('Its key', key, 'Its value', value.tag_name);
+                        console.log('Datapoints before each', dataPoints);
+                    });
+                    console.log('Datapoints after each', dataPoints);
+                    //chart.render();
+                });
+                console.log('Datapoints after If', dataPoints);
+
+            } else {
+
+                console.log('Task state ===>', data.task_state)
+                console.log('Task state ===>', data.task_id);
+                setTimeout(function () {
+                    getTaskResult(status_url);
+                }, 1000);
+                //setTimeout(getTaskResult(status_url), 1000);
+
+            }
+
+        });
+
+
+    };
+
+    function startTask(url) {
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/start',
+            data: {'url': url},
+            success: function (result, status, xhr) {
+                status_url = xhr.getResponseHeader('Location');
+
+                getTaskResult(status_url);
+
+
+            },
+            error: function () {
+                alert('Unexpected error');
+            }
+        });
+        console.log('Datapoints after STARTTask', dataPoints);
+
+
+    };
+
+    //chart.render();
+
+
+    /*
     $.getJSON("/tagscount", function (data) {
         $.each(data, function (key, value) {
             dataPoints.push({label: value.tag_name, y: value.tag_count});
@@ -66,6 +143,8 @@ window.onload = function () {
         });
         chart.render();
     });
+
+     */
 
 };
 
--- uenergoapp/viewst.py
+++ uenergoapp/viewst.py
@@ -4,23 +4,14 @@ from uenergoapp.tasks import mess, parse_html_tags
 from uenergoapp import app
 
 
-# from uenergoapp.adsbobject.adsbobject import ADSBDB, credentials
-# from bs4 import BeautifulSoup
-# import requests
-# import proj
-
-
-# from celery import Celery
-
-
-# @celery.task(bind=True)
-def simple_task(self):
-    return 'status'
-
-
 @app.route('/')
 def index():
     return render_template('ue_ajax.j2')
+
+
+@app.route('/s')
+def indexs():
+    return render_template('ue_canvasjs.j2')
 
 
 @app.route('/start', methods=['POST', 'GET'])
@@ -28,13 +19,6 @@ def start():
     url = request.form['url']
     tag_task = parse_html_tags.delay(url)
     return jsonify({'taskid': tag_task.id}), 202, {'Location': url_for('task_state', task_id=tag_task.id)}
-
-
-@app.route('/start2', methods=['POST', 'GET'])
-def start2(url='TestURL'):
-    url = request.form['name']
-
-    return jsonify({'url': url}), 202, {'Location': url}
 
 
 @app.route('/task_state/<task_id>', methods=['POST', 'GET'])
@@ -52,13 +36,3 @@ def task_result(task_id):
 
 if __name__ == '__main__':
     pass
-    tag_task = mess.delay()
-    print(type(tag_task.state), tag_task.state)
-
-'''res = mess.delay()
-# sleep(2)
-while res.state != 'SUCCESS':
-    print('Wait', res.state)
-    print(type(res.state), res.state)
-print(res.get(timeout=1))
-'''
