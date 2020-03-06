69f7de71e779a872de420d4c8e9affc49ec72a8f
mazuraktaras <53152688+mazuraktaras@users.noreply.github.com>
2020-03-06 10:27:11 +0200

    Rearrange project files.

--- uenergoapp/celapp.py
+++ uenergoapp/celapp.py
@@ -4,6 +4,7 @@ from celery import Celery
 celapp = Celery('uenergoapp', include=['uenergoapp.tasks'], task_ignore_result=False)
 celapp.config_from_object(uenergoapp.celeryconfig)
 
+# On Windows machine
 # celery -A uenergoapp.celapp worker -l info --pool=solo
 
 if __name__ == '__main__':
--- uetag.py
+++ uetag.py
@@ -2,7 +2,3 @@ from uenergoapp import app
 
 if __name__ == '__main__':
     app.run()
-    # print(__file__)
-    # set FLASK_ENV=development
-
-    # set FLASK_APP=uetag.py
