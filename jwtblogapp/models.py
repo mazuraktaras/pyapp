441668747473e07e5e504fb4d8d35423a9902bc9
mazuraktaras <53152688+mazuraktaras@users.noreply.github.com>
2020-04-08 16:23:26 +0300

    Commented

--- jwtblogapp/models.py
+++ jwtblogapp/models.py
@@ -3,26 +3,33 @@ from passlib.hash import pbkdf2_sha512
 
 
 class User(database.Model):
+    # The table with user credentials in database
     __tablename__ = 'blog_users'
 
+    # Description of a record in database
     id = database.Column(database.Integer, primary_key=True)
-    username = database.Column(database.String(128), unique=True)
-    password = database.Column(database.String(512))
+    username = database.Column(database.String(128), unique=True)  # column for username
+    password = database.Column(database.String(512))  # column for hashed password
     created_time = database.Column(database.DateTime)
 
     def store(self):
+        """
+        Add a new record to database and save it
+        """
         database.session.add(self)
         database.session.commit()
 
-    @classmethod
-    def del_all_users(cls):
-        database.session.query(cls).delete()
-        database.session.commit()
-
     def make_hash(self):
+        """
+        Make a hash of native password by sha512 algorithm
+        """
         self.password = pbkdf2_sha512.hash(self.password)
 
     def ensure_password(self, password):
+        """
+        Takes password and checks if it matches the hash
+        :rtype: bool
+        """
         return pbkdf2_sha512.verify(password, self.password)
 
     def __repr__(self):
@@ -30,61 +37,68 @@ class User(database.Model):
 
 
 class RevokedToken(database.Model):
+    # The table with revoked tokens in database
     __tablename__ = 'revoked_tokens'
 
+    # Description of a record in database
     id = database.Column(database.Integer, primary_key=True)
-    jti = database.Column(database.String(128))
-
-    # time = database.Column(database.DateTime, nullable=True)
+    jti = database.Column(database.String(128))  # column for JW token identifier
 
     def store(self):
+        """
+        Add a new record to database and save it
+        """
         database.session.add(self)
         database.session.commit()
 
 
 class Post(database.Model):
+    # The table with posts in database
     __tablename__ = 'posts'
 
     id = database.Column(database.Integer, primary_key=True)
     user_id = database.Column(database.Integer)
-    username = database.Column(database.String(128))
-    text = database.Column(database.UnicodeText)
-    likes = database.Column(database.Integer)
-    dislikes = database.Column(database.Integer)
+    username = database.Column(database.String(128))  # column for username
+    text = database.Column(database.UnicodeText)  # column for post text
+    likes = database.Column(database.Integer)  # column for post likes count
+    dislikes = database.Column(database.Integer)  # column for post dislikes count
     created_time = database.Column(database.DateTime)
 
     def store(self):
+        """
+        Add a new record to database and save it
+        """
         database.session.add(self)
         database.session.commit()
 
     @staticmethod
     def update():
-        database.session.commit()
-
-    @classmethod
-    def del_all_posts(cls):
-        database.session.query(cls).delete()
+        """
+        Update a record in database
+        """
         database.session.commit()
 
 
 class Rating(database.Model):
+    # The table with ratings in database
     __tablename__ = 'ratings'
 
     id = database.Column(database.Integer, primary_key=True)
-    user_id = database.Column(database.Integer)
-    post_like_id = database.Column(database.Integer)
-    post_dislike_id = database.Column(database.Integer)
+    user_id = database.Column(database.Integer)  # column for user id
+    post_like_id = database.Column(database.Integer)  # column for post id liked by user
+    post_dislike_id = database.Column(database.Integer)  # column for post id disliked by user
     created_time = database.Column(database.DateTime)
 
     def store(self):
+        """
+        Add a new record to database and save it
+        """
         database.session.add(self)
         database.session.commit()
 
     @staticmethod
     def update():
-        database.session.commit()
-
-    @classmethod
-    def del_all(cls):
-        database.session.query(cls).delete()
+        """
+        Update a record in database
+        """
         database.session.commit()
