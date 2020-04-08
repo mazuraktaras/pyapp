f1106a802d4e72924510ac4eb7c16ce98735e86f
mazuraktaras <53152688+mazuraktaras@users.noreply.github.com>
2020-04-08 16:35:03 +0300

    Commented

--- jwtblogapp/forms.py
+++ jwtblogapp/forms.py
@@ -4,16 +4,25 @@ from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(FlaskForm):
+    """
+    Describes the user credentials form in the frontend for signup and login
+    """
     username = StringField('Username', validators=[DataRequired(), Email()])
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign me up')
 
 
 class PostForm(FlaskForm):
+    """
+    Describes the post text form in the frontend for signup and login
+    """
     post_text = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField('Post')
 
 
 class RateForm(FlaskForm):
-    post_id = HiddenField()
-    like = HiddenField()
+    """
+    Describes the 'like and dislike forms' in the frontend for signup and login
+    """
+    post_id = HiddenField()  # a hidden field for render post_id value
+    like = HiddenField()  # a hidden field for render like 0 or 1 value
