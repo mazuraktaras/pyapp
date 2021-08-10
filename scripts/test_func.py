ebbd208b9fba02f8ec09d9f5db3eb572d4c46f5e
KonovalovAlexey <konovalov.netacad@gmail.com>
2021-08-10 16:26:41 +0300

    Change selenium to false

--- scripts/test_func.py
+++ scripts/test_func.py
@@ -26,8 +26,8 @@ class PyAppTest(unittest.TestCase):
         cls.driver.get(args.app_target_url)
 
     # Test for failing testing, uncomment it if need one test will be failed
-    # def test_fail(self):
-    #     self.assertEqual(1, 2)
+    def test_fail(self):
+        self.assertEqual(1, 2)
 
     def test_success(self):
         self.assertEqual(1, 1)
