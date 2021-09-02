d9524ce433fc16ef464c83087ed92220243c88e9
Taras Mazurak <Taras_Mazurak@epam.com>
2021-09-02 11:23:14 +0300

    Lambda stuff

--- testing_aws_lambda/dev_tg_health_check_lambda.py
+++ testing_aws_lambda/dev_tg_health_check_lambda.py
@@ -8,6 +8,7 @@ def lambda_handler(event, context):
     sns_topic_arn = 'arn:aws:sns:eu-north-1:188178296807:message-from-lambda'
     build_proj_name = 'pyapp'
 
+    # interval of checks in seconds to avoid retry timeout
     check_interval = 1
 
     health_status = None
@@ -15,31 +16,41 @@ def lambda_handler(event, context):
 
     # initialize elbv2 client
     elb_client = boto3.client('elbv2')
+
+    # initialize sns client
     sns_client = boto3.client('sns')
+
+    # initialize codebuild client
     code_build_client = boto3.client('codebuild')
 
-    checks_count = int((context.get_remaining_time_in_millis() / 1000 - 1) / check_interval)
-
+    # compute loops count accordingly remaining timeout
+    checks_count = int((context.get_remaining_time_in_millis() / 1000 - 2) / check_interval)
+    print(checks_count)
     # check target health state in loop
     while checks_count > 0:
 
+        # get target group health
         response = elb_client.describe_target_health(TargetGroupArn=target_group_arn)
 
+        # parse stat
         health_status = response['TargetHealthDescriptions'][0]['TargetHealth']['State']
 
         if health_status == 'healthy':
+            # start codeBuild project
             response = code_build_client.start_build(projectName=build_proj_name)
 
             message = f'CodeBuild project Selenium started successfully'
 
             break
-
+        # wait to avoid retry timeout
         time.sleep(check_interval)
-
+        # decrement cycle counter
         checks_count -= 1
 
+    # prepare message
     publish_object = f'Application health status is {health_status.upper()}. {message}'
 
+    # publish to topic
     sns_client.publish(TopicArn=sns_topic_arn,
                        Message=json.dumps(publish_object),
                        Subject='App health Notification')
