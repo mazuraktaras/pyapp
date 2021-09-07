500f007dd2ae8bc28f4675f34e0e6ea1aa1e0ab0
Taras Mazurak <Taras_Mazurak@epam.com>
2021-09-07 16:13:13 +0300

    Lambda stuff

---
+++ testing_aws_lambda/uat_asg_refresh_lambday.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+import os
+
+
+def lambda_handler(event, context):
+    # sns topic arn to email notification
+    sns_topic_arn = os.environ['SNS_TOPIC_ARN']
+    # ASG name to refresh
+    asg_name = os.environ['ASG_NAME']
+    # instance warmup in seconds.
+    instance_warmup = int(os.environ['WARM_UP'])
+
+    job_id = event['CodePipeline.job']['id']
+
+    # initialize autoscaling client
+    autoscaling_client = boto3.client('autoscaling')
+    # initialize sns client
+    sns_client = boto3.client('sns')
+    # initialize codepipeline client
+    code_pipeline_client = boto3.client('codepipeline')
+
+    # start ASG instance refresh
+    response = autoscaling_client.start_instance_refresh(AutoScalingGroupName=asg_name,
+                                                         Preferences={'InstanceWarmup': instance_warmup,
+                                                                      'MinHealthyPercentage': 90})
+
+    # prepare message to notify
+    publish_object = f'CodeBuild project Selenium ended successfully. ' \
+                     f'Instance refresh started in the {asg_name} autoscaling group.'
+
+    # publish message to email topic
+    sns_client.publish(TopicArn=sns_topic_arn,
+                       Message=json.dumps(publish_object),
+                       Subject='CodeBuild project Selenium Notification')
+
+    response = code_pipeline_client.put_job_success_result(jobId=job_id)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'Lambda function {context.function_name} completed.')
+    }
