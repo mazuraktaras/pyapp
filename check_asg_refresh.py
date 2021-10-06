cbc706e9f9a39a04dd223bd2c503d90672accb89
Taras Mazurak <Taras_Mazurak@epam.com>
2021-10-07 00:02:05 +0300

    Test python script

--- check_asg_refresh.py
+++ check_asg_refresh.py
@@ -13,29 +13,33 @@ status = 'Successful'
 
 print(status)
 print(time.perf_counter())
+start_time = time.perf_counter()
 
 for i in range(5):
     print('From script')
     print(time.perf_counter())
+    time.sleep(1)
+stop_time = time.perf_counter()
+print(stop_time - start_time)
 # while True:
 #     print('From script')
 
-while status == 'Successful':
-    print(time.perf_counter())
-
-    if time.perf_counter() > TIMEOUT:
-        print('I am gonna exiting!')
-        print(f'Autoscaling group {ASG_NAME} instance refresh check exited with timeout {TIMEOUT} sec.')
-        exit(1)
+# while status == 'Successful':
+#     print(time.perf_counter())
 #
-    response = client.describe_instance_refreshes(
-        AutoScalingGroupName=ASG_NAME,
-        InstanceRefreshIds=[INST_REFRESH_ID],
-        MaxRecords=1
-    )
-
-    print('Wait ASG refresh.....')
-    time.sleep(2)
+#     if time.perf_counter() > TIMEOUT:
+#         print('I am gonna exiting!')
+#         print(f'Autoscaling group {ASG_NAME} instance refresh check exited with timeout {TIMEOUT} sec.')
+#         exit(1)
+# #
+#     response = client.describe_instance_refreshes(
+#         AutoScalingGroupName=ASG_NAME,
+#         InstanceRefreshIds=[INST_REFRESH_ID],
+#         MaxRecords=1
+#     )
+#
+#     print('Wait ASG refresh.....')
+#     time.sleep(2)
 #
 #     # pprint(response)
 #     # pprint(response['InstanceRefreshes'][0]['Status'])
