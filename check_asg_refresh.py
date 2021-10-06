9908565f84aa079523c5e8b0afc99ade5f32de11
Taras Mazurak <Taras_Mazurak@epam.com>
2021-10-06 23:39:11 +0300

    Test python script

--- check_asg_refresh.py
+++ check_asg_refresh.py
@@ -1,6 +1,7 @@
 import time
 from pprint import pprint
-#import boto3
+
+# import boto3
 
 ASG_NAME = 'DEV-asg'
 INST_REFRESH_ID = '3d97896d-cb83-46da-a8db-5eb1cfe4f705'
@@ -22,11 +23,10 @@ for i in range(5):
 while status == 'Successful':
     print(time.perf_counter())
 
-#
-#     # if time.perf_counter() > TIMEOUT:
-#     #     print('I am gonna exiting!')
-#     #     print(f'Autoscaling group {ASG_NAME} instance refresh check exited with timeout {TIMEOUT} sec.')
-#     #     exit(1)
+    if time.perf_counter() > TIMEOUT:
+        print('I am gonna exiting!')
+        print(f'Autoscaling group {ASG_NAME} instance refresh check exited with timeout {TIMEOUT} sec.')
+        exit(1)
 #
 #     # response = client.describe_instance_refreshes(
 #     #     AutoScalingGroupName=ASG_NAME,
