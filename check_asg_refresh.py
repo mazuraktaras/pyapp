c82f497a2e1b7e881f4358a0c68b76205ac9adda
Taras Mazurak <Taras_Mazurak@epam.com>
2021-10-07 10:50:52 +0300

    Test python script

--- check_asg_refresh.py
+++ check_asg_refresh.py
@@ -7,6 +7,7 @@ import boto3
 ASG_NAME = 'DEV-asg'
 INST_REFRESH_ID = '17b9585b-d570-40f1-aed2-39582809885e'
 TIMEOUT = 60
+TARG_GROUP_ARN = 'arn:aws:elasticloadbalancing:eu-north-1:188178296807:targetgroup/DEV-tg/8e9797c856a1f915'
 
 client = boto3.client('autoscaling')
 
@@ -39,4 +40,25 @@ while status != 'Successfu':
 
     if status == 'Successful':
         print('Do stuff')
+        print(f'Check DEV-tg health status..... Time elapsed {elapsed_time} sec.')
+
+        # initialize elbv2 client
+        elb_client = boto3.client('elbv2')
+
+        while True:
+
+            if time.perf_counter() - start_time > TIMEOUT:
+                print('I am gonna exiting!')
+                print(f'Autoscaling group {ASG_NAME} instance refresh check exited with timeout {TIMEOUT} sec.')
+                exit(1)
+
+            # get target group health
+            response = elb_client.describe_target_health(TargetGroupArn=TARG_GROUP_ARN)
+
+            # parse status
+            health_status = response['TargetHealthDescriptions'][0]['TargetHealth']['State']
+
+            print(health_status)
+            time.sleep(2)
+
         # break
